daee11d8ab153210d603602bc34f441482ca308d
Bram Jetten <mail@bramjetten.nl>
2013-04-24 11:21:43 +0200

    Manage all plugins

--- app/models/spina/ability.rb
+++ app/models/spina/ability.rb
@@ -10,6 +10,10 @@ module Spina
         can :manage, Spina::Photo
         can :manage, Spina::Account
         can :manage, Spina::Inquiry
+
+        Spina.plugins.each do |plugin|
+          can :manage, "Spina::#{plugin.class_name}".constantize
+        end
       end
     end
   end
