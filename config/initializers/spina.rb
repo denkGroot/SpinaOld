2cd0b476cabbef3e8cccd4fff1d14ca3562e4f42
Bram Jetten <mail@bramjetten.nl>
2013-04-29 17:10:43 +0200

    Spina plugins

--- config/initializers/spina.rb
+++ config/initializers/spina.rb
@@ -6,3 +6,13 @@ Spina.page_parts = config['page_parts']
 Spina.custom_pages = config['page_parts'].delete_if { |page_part| page_part == "default" }.keys
 
 Spina.plugins = Array.new
+
+config['plugins'].each do |plugin_name, plugin_config|
+  plugin = Spina::Plugin.new
+  plugin.name = plugin_name
+  plugin.class_name = plugin_config['class_name']
+  plugin.controller = plugin_config['controller']
+  plugin.pictos_icon = plugin_config['pictos_icon']
+
+  Spina.plugins << plugin
+end
