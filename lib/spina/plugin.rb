d6c71d7ce294d54aea1f131cd0f3f2b3a06ba591
Bram Jetten <mail@bramjetten.nl>
2013-05-04 16:33:15 +0200

    Plugin

--- lib/spina/plugin.rb
+++ lib/spina/plugin.rb
@@ -1,6 +1,6 @@
 module Spina
   class Plugin
-    attr_accessor :name, :class_name, :controller, :pictos_icon
+    attr_accessor :name, :class_name, :controller, :pictos_icon, :path
 
     def to_s
       name
