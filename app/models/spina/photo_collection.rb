835d780bfd77782769ea6d052d2077704e16f527
Bram Jetten <mail@bramjetten.nl>
2013-09-05 14:23:14 +0200

    Eindelijk

--- app/models/spina/photo_collection.rb
+++ app/models/spina/photo_collection.rb
@@ -4,13 +4,17 @@ module Spina
     has_one :page_part, as: :page_partable
     has_and_belongs_to_many :photos, join_table: 'spina_photo_collections_photos'
 
-    attr_accessible :photos, :photo_ids
+    attr_accessible :photos, :photo_tokens
+    attr_reader :photo_tokens
     accepts_nested_attributes_for :photos, :allow_destroy => true
+
+    def photo_tokens=(ids)
+      self.photo_ids = ids.split(",")
+    end
 
     alias_method :old_update_attributes, :update_attributes
     def update_attributes(attributes)
       self.photos.clear if attributes.reject{|key,value| key == "id" }.blank?
-      attributes[:photo_ids] = attributes[:photo_ids].split(",") if attributes[:photo_ids].instance_of? String
       old_update_attributes(attributes)
     end
 
