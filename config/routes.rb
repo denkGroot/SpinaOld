40b934ff7207e47a3ab70deb4f066630e62be797
Bram Jetten <mail@bramjetten.nl>
2013-09-08 20:14:02 +0200

    Changes

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,12 @@ Spina::Engine.routes.draw do
 
     resources :pages do
       post :sort, on: :collection
+      resources :page_parts do
+        member do
+          get 'select'
+          post 'insert'
+        end
+      end
     end
 
     resources :attachments do
