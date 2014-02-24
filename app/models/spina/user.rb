f8efe5aece1649ba36c2ef56211f3f1c4dd339b3
Bram Jetten <mail@bramjetten.nl>
2014-02-24 13:56:38 +0100

    Validation custom made

--- app/models/spina/user.rb
+++ app/models/spina/user.rb
@@ -4,7 +4,7 @@ module Spina
 
     validates_presence_of :name, :email
     validates_presence_of :password, on: :create
-    validates_uniqueness_of :email
+    validate :uniqueness_of_email
     validates :email, format: { with:/\A[^@]+@[^@]+\z/, message: 'is geen geldig emailadres' }
 
     def admin?
@@ -18,6 +18,14 @@ module Spina
     def update_last_logged_in!
       self.last_logged_in = Time.now
       self.save!
+    end
+
+    private
+
+    def uniqueness_of_email
+      if email_changed? && User.where(email: email).exists?
+        errors.add(:email, I18n.t('errors.messages.taken'))
+      end
     end
   end
 end
