12db06307a0dce7ae4099ca4181656205d9f385f
Bram Jetten <mail@bramjetten.nl>
2014-04-11 12:59:35 +0200

    Retina images

--- app/helpers/spina/application_helper.rb
+++ app/helpers/spina/application_helper.rb
@@ -34,6 +34,11 @@ module Spina
       @current_account ||= Account.first
     end
 
+    def image_tag_with_at2x(name_at_1x, options={})
+      name_at_2x = name_at_1x.gsub(%r{\.\w+$}, '@2x\0')
+      image_tag(name_at_1x, options.merge("data-at2x" => asset_path(name_at_2x)))
+    end
+
     def error_explanation!(resource)
       return "" if resource.errors.empty?
 
